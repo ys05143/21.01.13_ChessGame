830c9bd813f229fce63923ad0fe15e9954837bb6
HEO JIHUN <ys05143@gmail.com>
2021-01-15 01:14:25 +0900

    test 1

--- app/src/main/java/com/example/chessgame/MainActivity.java
+++ app/src/main/java/com/example/chessgame/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,4 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public void onButtonClicked(View v){
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
-}
+
+
+
+
+    }
