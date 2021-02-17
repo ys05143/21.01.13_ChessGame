f206218ad35675d5111c81440ac5eeeca2784316
beanzinu <wlsdn1372@naver.com>
2021-02-17 15:42:08 +0900

    2-17 demo1

--- app/src/main/java/com/example/chessgame/IntroActivity.java
+++ app/src/main/java/com/example/chessgame/IntroActivity.java
@@ -18,18 +18,17 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotation);
-        ImageView rotate1 = (ImageView)findViewById(R.id.rook_anim) ;
-        ImageView rotate2 = (ImageView)findViewById(R.id.knight_anim) ;
-        ImageView rotate3 = (ImageView)findViewById(R.id.bishp_anim) ;
-        ImageView rotate4 = (ImageView)findViewById(R.id.queen_anim) ;
-        ImageView rotate5 = (ImageView)findViewById(R.id.king_anim) ;
-        ImageView rotate6 = (ImageView)findViewById(R.id.pawn_anim) ;
-        rotate1.startAnimation(animation);
-        rotate2.startAnimation(animation);
-        rotate3.startAnimation(animation);
-        rotate4.startAnimation(animation);
-        rotate5.startAnimation(animation);
-        rotate6.startAnimation(animation);
+        ImageView Brotate1 = (ImageView)findViewById(R.id.rook_anim) ;
+        Brotate1.startAnimation(animation);
+
+        ImageView Wrotate1 = (ImageView)findViewById(R.id.rook_anim2) ;
+        Wrotate1.startAnimation(animation);
+
+        ImageView Protate1 = (ImageView)findViewById(R.id.rook_anim3) ;
+        Protate1.startAnimation(animation);
+
+        ImageView Protate2 = (ImageView)findViewById(R.id.rook_anim4) ;
+        Protate2.startAnimation(animation);
 
         Button b1=(Button)findViewById(R.id.oneP_Game_w);
         Button b2=(Button)findViewById(R.id.oneP_Game_b);
